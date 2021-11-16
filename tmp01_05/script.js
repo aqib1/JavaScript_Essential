f8aebbc9ca06e02bd00dbe5c6a5e8cea8f34eb8d
aqib1 <aqibbutt3078@gmail.com>
2021-11-17 00:40:50 +0100

    JavaScript objects

--- tmp01_05/script.js
+++ tmp01_05/script.js
@@ -1,10 +1,20 @@
 const backpack = {
   volume: 30,
-  setVolume: (volume) => {
+  strapSize: {
+    width: 11,
+    height: 12,
+  },
+  setVolume: function (volume) {
     console.log(this);
     this.volume = volume;
   },
 };
 
 backpack.setVolume(4);
-console(backpack.volume);
+console.log(backpack.volume);
+console.log("Strap size is: ", backpack.strapSize); // dot notation
+console.log("Width size is:", backpack.strapSize.width); // dot notation
+console.log("Strap size as bracket notation: ", backpack["strapSize"]);
+
+const query = "volume";
+console.log("volume using bracket notation: ", backpack[query]);
